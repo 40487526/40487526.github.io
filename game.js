53780d683424e2466ec293f2e50e4b7d9ced5bc2
40487526 <103508204+40487526@users.noreply.github.com>
2022-04-27 17:34:01 +0100

    finalthing

--- game.js
+++ game.js
@@ -6,83 +6,76 @@ var a = document.getElementById('next_btn');
 
 var class_name1 = 'correct';
 var class_name2 = 'incorrect';
-elements1=document.getElementsByClassName(class_name1)
-elements2=document.getElementsByClassName(class_name2)
+elements1 = document.getElementsByClassName(class_name1)
+elements2 = document.getElementsByClassName(class_name2)
 
 let currentQuestion = {};
-let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
 
-let questions = [
-    {
+let questions = [{
         question: 'Is this cake or a shoe?',
-      choice1: "Cake",
-      choice2: "Shoe",
-      videoLinkShort: "shoecake_start.mp4",
-      videoLinkFull: "shoecake_full.mp4",
-      answer: 1
-        
-     },
-     {
+        choice1: "Cake",
+        choice2: "Shoe",
+        videoLinkShort: "shoecake_start.mp4",
+        videoLinkFull: "shoecake_full.mp4",
+        answer: 1
+
+    },
+    {
         question: 'Is this cake or some loo roll?',
-      choice1: "Cake",
-      choice2: "Loo Roll",
-      videoLinkShort: "loorollcake_start.mp4",
-      videoLinkFull: "loorollcake_full.mp4",
-      answer: 1
+        choice1: "Cake",
+        choice2: "Loo Roll",
+        videoLinkShort: "loorollcake_start.mp4",
+        videoLinkFull: "loorollcake_full.mp4",
+        answer: 1
 
-     },
-     {
+    },
+    {
         question: 'Is this cake or a bar of soap?',
-      choice1: "Cake",
-      choice2: "Soap",
-      videoLinkShort: "soapreal_start.mp4",
-      videoLinkFull: "soapreal_full.mp4",
-      answer: 2
+        choice1: "Cake",
+        choice2: "Soap",
+        videoLinkShort: "soapreal_start.mp4",
+        videoLinkFull: "soapreal_full.mp4",
+        answer: 2
 
-     },
-     {
+    },
+    {
         question: 'Is this cake or a banana',
-      choice1: "Cake",
-      choice2: "Banana",
-      videoLinkShort: "bananareal_start.mp4",
-      videoLinkFull: "bananareal_full.mp4",
-      answer: 1
+        choice1: "Cake",
+        choice2: "Banana",
+        videoLinkShort: "bananareal_start.mp4",
+        videoLinkFull: "bananareal_full.mp4",
+        answer: 1
 
-     }
-   ];
-   
-   //constants
-   const CORRECT_BONUS = 10;
-   const MAX_QUESTIONS = 4;
+    }
+];
 
-   startGame = () => {
+const MAX_QUESTIONS = 4;
+
+startGame = () => {
 
     questionCounter = 0;
     score = 0;
     availableQuestions = [...questions];
     getNewQuestion();
-   };
+};
 
-   getNewQuestion = () => {
+getNewQuestion = () => {
     nextButton.classList.add("hidden")
-    for(element of elements1){
+    for (element of elements1) {
         element.classList.remove(class_name1)
-      }
-      for(element of elements2){
+    }
+    for (element of elements2) {
         element.classList.remove(class_name2)
-      }
-console.log(questionCounter);
+    }
+    console.log(questionCounter);
 
     console.log(questionCounter);
     questionCounter++;
-    
-    // if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
-    //     nextButton.innerText = "Finish"
-    //    }
-   
+
+
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     console.log(questionCounter);
@@ -92,57 +85,58 @@ console.log(questionCounter);
 
     choices.forEach((choice) => {
         const number = choice.dataset['number'];
-       choice.innerText = currentQuestion['choice' + number];
+        choice.innerText = currentQuestion['choice' + number];
     });
-    
+
     availableQuestions.splice(questionIndex, 1);
-    acceptingAnswers = true;
+
     endJS();
 };
 
 function endJS() {
+    let testFlag = false;
 
+    choices.forEach((choice) => {
 
-choices.forEach((choice) => {
-    choice.addEventListener('click', e => {
+        choice.addEventListener('click', e => {
+            if (testFlag === true) {
+                return; //https://masteringjs.io/tutorials/fundamentals/foreach-break
+            }
 
-        if (!acceptingAnswers) return;
-        
-        acceptingAnswers = false;
-        const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset['number'];
+            const selectedChoice = e.target;
+            const selectedAnswer = selectedChoice.dataset['number'];
 
-        const classToApply = selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
-       
-        selectedChoice.classList.add(classToApply);
+            const classToApply = selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
 
-        vidUrlSrc = currentQuestion.videoLinkFull;
-        vidUrl.src = vidUrlSrc;
-        
-        console.log(questionCounter);
-    nextButton.classList.remove("hidden");
+            selectedChoice.classList.add(classToApply);
+            testFlag = true;
+            endFunction();
+        });
 
-    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
-        nextButton.innerText = "Finish"
-        goHome()
-        
-        } else {
-            nextButton.addEventListener('click', getNewQuestion);
+        function endFunction() {
+
+            vidUrlSrc = currentQuestion.videoLinkFull;
+            vidUrl.src = vidUrlSrc;
+            nextButton.classList.remove("hidden");
+
+            if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+                nextButton.innerText = "Finish"
+                goHome()
+
+            } else {
+                nextButton.addEventListener('click', getNewQuestion);
+            }
+
         }
-    
- 
+
+
     });
-});
+} //end of endJS
 function goHome() {
     nextButton.addEventListener('click', e => {
-    window.location.assign("/index.html");
-    
-});
-}}
+        window.location.assign("/index.html");
 
+    });
+}
 
 startGame();
-// setTimeout(() => {
-    //     selectedChoice.classList.remove(classToApply);
-    //     getNewQuestion();
-    //   }, 1000);
